41bac71036c846be4f7e3757743a39dab32ed1a3
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-06-09 11:52:47 -0300

    Update urls.py

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import hello_home
 from .views import hello_blog
+from .views import post_detail
 
 urlpatterns = [
     path('', hello_blog),
     path('blog/', hello_blog),
+    path('post/<int:id>/', post_detail)
 ]
 
