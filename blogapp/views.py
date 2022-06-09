bde5d6912f227d261f491701d648eae5bd4f77ed
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-06-09 12:00:12 -0300

    Update views.py

--- blogapp/views.py
+++ blogapp/views.py
@@ -11,3 +11,7 @@ def hello_blog(request):
     list_posts = Post.objects.all()
     data = {'posts': list_posts }
     return render(request, 'blog.html', data)
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'post_detail.html', {'post': post})
